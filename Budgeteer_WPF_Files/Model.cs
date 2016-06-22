fb449891d2da88e383b1ddcc4c4bb25a2597acbc
Aleks Angelov <aleks_angelov@mail.com>
2016-06-22 11:20:11 +0300

    InitialModel

---
+++ Budgeteer_WPF_Files/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Budgeteer_WPF_Files
+{
+    internal abstract class Transaction
+    {
+        protected DateTime Date;
+        protected string Person;
+        protected float Amount;
+        protected string Note;
+    }
+
+    internal class Debit : Transaction
+    {
+        public enum Categories
+        {
+            Food = 0,
+            Rent = 1
+        }
+    }
+
+    internal class Credit : Transaction
+    {
+        public enum Categories
+        {
+            Salary = 0,
+            Bonus = 1
+        }
+    }
+}
