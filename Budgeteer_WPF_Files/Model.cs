ffea25961fa5d34e9d9e82e6d178bc82a8413954
Aleks Angelov <aleks_angelov@mail.com>
2016-06-22 12:07:52 +0300

    Model Update

--- Budgeteer_WPF_Files/Model.cs
+++ Budgeteer_WPF_Files/Model.cs
@@ -5,27 +5,56 @@ namespace Budgeteer_WPF_Files
 {
     internal abstract class Transaction
     {
+        public static List<string> People = new List<string>();
+
         protected DateTime Date;
         protected string Person;
+        protected string Category;
         protected float Amount;
         protected string Note;
+
+        protected Transaction(DateTime d, string p, string c, float a, string n = "")
+        {
+            Date = d;
+            Person = p;
+            Category = c;
+            Amount = a;
+            Note = n;
+        }
     }
 
     internal class Debit : Transaction
     {
-        public enum Categories
+        public static List<string> DebitCategories = new List<string>
         {
-            Food = 0,
-            Rent = 1
+            "Groceries",
+            "Personal Care",
+            "Transportation",
+            "Entertainment",
+            "Restaurants",
+            "Utilities",
+            "Loans",
+            "Rent",
+            "Clothing",
+            "Healthcare",
+            "Travel",
+            "Equipment",
+            "Miscellaneous"
+        };
+
+        public Debit(DateTime d, string p, string c, float a, string n = "")
+            : base(d, p, c, a, n)
+        {
         }
     }
 
     internal class Credit : Transaction
     {
-        public enum Categories
+        public static List<string> CreditCategories = new List<string> {"Salary", "Bonuses", "Dividends", "Other"};
+
+        public Credit(DateTime d, string p, string c, float a, string n = "")
+            : base(d, p, c, a, n)
         {
-            Salary = 0,
-            Bonus = 1
         }
     }
 }
