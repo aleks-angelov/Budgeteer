d0c0b2d01e84f3ebaf3568f37f230099627798bd
Aleks Angelov <aleks_angelov@mail.com>
2016-06-23 17:09:54 +0300

    Spending Tab Done

--- Budgeteer_WPF_Files/MainWindow.xaml.cs
+++ Budgeteer_WPF_Files/MainWindow.xaml.cs
@@ -14,48 +14,42 @@ namespace Budgeteer_WPF_Files
     {
         private static readonly ObservableCollection<Transaction> records = new ObservableCollection<Transaction>
         {
-            new Debit(DateTime.Today.AddMonths(-12), "Aleks Angelov", "Groceries", 100.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-11), "Aleks Angelov", "Groceries", 50.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-10), "Aleks Angelov", "Groceries", 40.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-9), "Aleks Angelov", "Groceries", 10.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-8), "Aleks Angelov", "Groceries", 20.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-7), "Aleks Angelov", "Groceries", 30.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-6), "Aleks Angelov", "Groceries", 160.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-5), "Aleks Angelov", "Groceries", 150.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-4), "Aleks Angelov", "Groceries", 140.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-3), "Aleks Angelov", "Groceries", 110.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-2), "Aleks Angelov", "Groceries", 120.05, "Lunch and dinner"),
-            new Debit(DateTime.Today.AddMonths(-1), "Aleks Angelov", "Groceries", 130.05, "Lunch and dinner"),
-            new Debit(DateTime.Today, "Aleks Angelov", "Groceries", 60.05, "Lunch and dinner"),
-
-            new Credit(DateTime.Today.AddMonths(-12), "Aleks Angelov", "Salary", 109.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-11), "Aleks Angelov", "Salary", 48.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-10), "Aleks Angelov", "Salary", 37.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-9), "Aleks Angelov", "Salary", 16.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-8), "Aleks Angelov", "Salary", 15.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-7), "Aleks Angelov", "Salary", 24.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-6), "Aleks Angelov", "Salary", 164.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-5), "Aleks Angelov", "Salary", 145.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-4), "Aleks Angelov", "Salary", 136.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-3), "Aleks Angelov", "Salary", 117.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-2), "Aleks Angelov", "Salary", 118.05, "Lunch and dinner"),
-            new Credit(DateTime.Today.AddMonths(-1), "Aleks Angelov", "Salary", 129.05, "Lunch and dinner"),
-            new Credit(DateTime.Today, "Aleks Angelov", "Salary", 60.05, "Lunch and dinner")
+            new Debit(DateTime.Today.AddMonths(-12), "Aleks Angelov", "Groceries", 100.05, "A"),
+            new Debit(DateTime.Today.AddMonths(-11), "Boris Ruskov", "Personal Care", 50.05, "B"),
+            new Debit(DateTime.Today.AddMonths(-10), "Mariya Stancheva", "Transportation", 40.05, "C"),
+            new Debit(DateTime.Today.AddMonths(-9), "Aleks Angelov", "Entertainment", 10.05, "D"),
+            new Debit(DateTime.Today.AddMonths(-8), "Boris Ruskov", "Groceries", 20.05, "E"),
+            new Debit(DateTime.Today.AddMonths(-7), "Mariya Stancheva", "Personal Care", 30.05, "F"),
+            new Debit(DateTime.Today.AddMonths(-6), "Aleks Angelov", "Transportation", 160.05, "G"),
+            new Debit(DateTime.Today.AddMonths(-5), "Boris Ruskov", "Entertainment", 150.05, "H"),
+            new Debit(DateTime.Today.AddMonths(-4), "Mariya Stancheva", "Groceries", 140.05, "I"),
+            new Debit(DateTime.Today.AddMonths(-3), "Aleks Angelov", "Personal Care", 110.05, "J"),
+            new Debit(DateTime.Today.AddMonths(-2), "Boris Ruskov", "Transportation", 120.05, "K"),
+            new Debit(DateTime.Today.AddMonths(-1), "Mariya Stancheva", "Entertainment", 130.05, "L"),
+            new Debit(DateTime.Today, "Aleks Angelov", "Groceries", 60.05),
+            new Credit(DateTime.Today.AddMonths(-12), "Boris Ruskov", "Salary", 109.05),
+            new Credit(DateTime.Today.AddMonths(-11), "Mariya Stancheva", "Bonuses", 48.05, "a"),
+            new Credit(DateTime.Today.AddMonths(-10), "Aleks Angelov", "Dividends", 37.05, "b"),
+            new Credit(DateTime.Today.AddMonths(-9), "Boris Ruskov", "Other", 16.05, "c"),
+            new Credit(DateTime.Today.AddMonths(-8), "Mariya Stancheva", "Salary", 15.05, "d"),
+            new Credit(DateTime.Today.AddMonths(-7), "Aleks Angelov", "Bonuses", 24.05, "e"),
+            new Credit(DateTime.Today.AddMonths(-6), "Boris Ruskov", "Dividends", 164.05, "f"),
+            new Credit(DateTime.Today.AddMonths(-5), "Mariya Stancheva", "Other", 145.05, "g"),
+            new Credit(DateTime.Today.AddMonths(-4), "Aleks Angelov", "Salary", 136.05, "h"),
+            new Credit(DateTime.Today.AddMonths(-3), "Boris Ruskov", "Bonuses", 117.05, "i"),
+            new Credit(DateTime.Today.AddMonths(-2), "Mariya Stancheva", "Dividends", 118.05, "j"),
+            new Credit(DateTime.Today.AddMonths(-1), "Aleks Angelov", "Other", 129.05, "k"),
+            new Credit(DateTime.Today, "Boris Ruskov", "Salary", 60.05, "l")
         };
 
-        private void RecordsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
-        {
-            LoadBudgetBalanceData();
-            LoadSpendingDistributionData();
-        }
-
         private static readonly IEnumerable<Debit> debitQuery = from record in records
-                                                                where record.Type == "Debit"
-                                                                select record as Debit;
+            where record.Type == "Debit"
+            select record as Debit;
 
         private static readonly IEnumerable<Credit> creditQuery = from record in records
-                                                                  where record.Type == "Credit"
-                                                                  select record as Credit;
+            where record.Type == "Credit"
+            select record as Credit;
+
 
         public MainWindow()
         {
@@ -64,10 +58,23 @@ namespace Budgeteer_WPF_Files
             records.CollectionChanged += RecordsOnCollectionChanged;
         }
 
+        private void RecordsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            ReloadData();
+        }
+
+
+        private void ReloadData()
+        {
+            ReloadOverviewData();
+            ReloadSpendingData();
+            // ReloadIncomeData();
+        }
+
         private void mainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             SetupOverviewTab();
-            //SetupSpendingTab();
+            SetupSpendingTab();
             //SetupIncomeTab();
             //SetupSettingsTab();
         }
@@ -88,7 +95,7 @@ namespace Budgeteer_WPF_Files
         {
             Transaction newTransaction;
             if (RadioButtonDebit.IsChecked == true)
-                newTransaction = new Debit(DatePickerAdd.DisplayDate.Date, ComboBoxAddPerson.Text,
+                newTransaction = new Debit(DatePickerAdd.DisplayDate, ComboBoxAddPerson.Text,
                     ComboBoxAddCategory.Text,
                     double.Parse(TextBoxAddAmount.Text), TextBoxAddNote.Text);
             else
--- Budgeteer_WPF_Files/OverviewTab.cs
+++ Budgeteer_WPF_Files/OverviewTab.cs
@@ -13,14 +13,14 @@ namespace Budgeteer_WPF_Files
             ComboBoxAddCategory.ItemsSource = Debit.DebitCategories;
             DataGridOverview.ItemsSource = records;
 
-            LoadBudgetBalanceData();
-            LoadSpendingDistributionData();
+            ReloadOverviewData();
         }
 
-        private void LoadBudgetBalanceData()
+        private void ReloadOverviewData()
         {
             LoadIncomeData();
             LoadSpendingData();
+            LoadSpendingDistributionData();
         }
 
         private void LoadIncomeData()
@@ -28,17 +28,17 @@ namespace Budgeteer_WPF_Files
             List<Credit> incomeRecords = creditQuery.ToList();
 
             IOrderedEnumerable<IGrouping<string, double>> incomeRecordsByMonth = from record in incomeRecords
-                                                                                 where record.Date.AddMonths(7) > DateTime.Today
-                                                                                 group record.Amount by record.Date.ToString("yyyy/MM")
+                where record.Date.AddMonths(7) > DateTime.Today
+                group record.Amount by record.Date.ToString("yyyy/MM")
                 into monthlyRecords
-                                                                                 orderby monthlyRecords.Key
-                                                                                 select monthlyRecords;
+                orderby monthlyRecords.Key
+                select monthlyRecords;
 
             List<KeyValuePair<string, double>> incomeData = new List<KeyValuePair<string, double>>();
             foreach (IGrouping<string, double> period in incomeRecordsByMonth)
                 incomeData.Add(new KeyValuePair<string, double>(period.Key, period.Sum()));
 
-            ((ColumnSeries)ChartOverviewLeft.Series[0]).ItemsSource = incomeData;
+            ((ColumnSeries) ChartOverviewLeft.Series[0]).ItemsSource = incomeData;
         }
 
         private void LoadSpendingData()
@@ -46,17 +46,17 @@ namespace Budgeteer_WPF_Files
             List<Debit> spendingRecords = debitQuery.ToList();
 
             IOrderedEnumerable<IGrouping<string, double>> spendingRecordsByMonth = from record in spendingRecords
-                                                                                   where record.Date.AddMonths(7) > DateTime.Today
-                                                                                   group record.Amount by record.Date.ToString("yyyy/MM")
+                where record.Date.AddMonths(7) > DateTime.Today
+                group record.Amount by record.Date.ToString("yyyy/MM")
                 into monthlyRecords
-                                                                                   orderby monthlyRecords.Key
-                                                                                   select monthlyRecords;
+                orderby monthlyRecords.Key
+                select monthlyRecords;
 
             List<KeyValuePair<string, double>> spendingData = new List<KeyValuePair<string, double>>();
             foreach (IGrouping<string, double> period in spendingRecordsByMonth)
                 spendingData.Add(new KeyValuePair<string, double>(period.Key, period.Sum()));
 
-            ((ColumnSeries)ChartOverviewLeft.Series[1]).ItemsSource = spendingData;
+            ((ColumnSeries) ChartOverviewLeft.Series[1]).ItemsSource = spendingData;
         }
 
         private void LoadSpendingDistributionData()
@@ -67,13 +67,13 @@ namespace Budgeteer_WPF_Files
             foreach (string spendingCategory in Debit.DebitCategories)
             {
                 double categoryTotal = (from record in spendingRecords
-                                        where record.Category == spendingCategory && record.Date.AddMonths(7) > DateTime.Today
-                                        select record.Amount).Sum();
+                    where record.Category == spendingCategory && record.Date.AddMonths(7) > DateTime.Today
+                    select record.Amount).Sum();
 
                 if (categoryTotal > 0)
                     spendingDistributionData.Add(new KeyValuePair<string, double>(spendingCategory, categoryTotal));
             }
-            ((PieSeries)ChartOverviewRight.Series[0]).ItemsSource = spendingDistributionData;
+            ((PieSeries) ChartOverviewRight.Series[0]).ItemsSource = spendingDistributionData;
         }
     }
 }
