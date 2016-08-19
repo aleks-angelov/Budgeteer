939c0d97a45898d28799b26aeacce2bd26dd409e
Aleks Angelov <aleks_angelov@mail.com>
2016-08-19 16:58:08 +0300

    Angular 2 setup

---
+++ Budgeteer_Web_Angular/gulpfile.js
@@ -0,0 +1,23 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('restore', function() {
+    gulp.src([
+        'node_modules/@angular/**/*.js',
+        'node_modules/angular2-in-memory-web-api/*.js',
+        'node_modules/rxjs/**/*.js',
+        'node_modules/systemjs/dist/*.js',
+        'node_modules/zone.js/dist/*.js',
+        'node_modules/core-js/client/*.js',
+        'node_modules/reflect-metadata/reflect.js',
+        'node_modules/bootstrap/dist/**/*.*'
+    ]).pipe(gulp.dest('./wwwroot/libs'));
+});
