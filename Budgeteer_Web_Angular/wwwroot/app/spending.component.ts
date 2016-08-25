1ba36a60a48026bdfd4ead97f679fc8cd4c4983a
Aleks Angelov <aleks_angelov@mail.com>
2016-08-25 11:42:11 +0300

    spending charts test

--- Budgeteer_Web_Angular/wwwroot/app/spending.component.ts
+++ Budgeteer_Web_Angular/wwwroot/app/spending.component.ts
@@ -36,29 +36,206 @@ export class SpendingComponent implements OnInit {
         this.getFormData();
         this.getTransactions();
 
-        const chart = new Highcharts.Chart({
+        const topLeftChart = new Highcharts.Chart({
             chart: {
-                type: "bar",
-                renderTo: "myChart"
+                type: "column",
+                renderTo: "myTopLeftChart"
             },
             title: {
-                text: "Fruit Consumption"
+                text: "Monthly Average Rainfall"
             },
             xAxis: {
-                categories: ["Apples", "Bananas", "Oranges"]
+                categories: [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec"
+                ],
+                crosshair: true
             },
             yAxis: {
+                min: 0,
                 title: {
-                    text: "Fruit eaten"
+                    text: "Rainfall (mm)"
+                }
+            },
+            tooltip: {
+                pointFormat: "{series.name}: <b>{point.y:.1f} mm</b>"
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
                 }
             },
             series: [
                 {
-                    name: "Jane",
-                    data: [1, 0, 4]
+                    name: "Tokyo",
+                    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
                 }, {
-                    name: "John",
-                    data: [5, 7, 3]
+                    name: "New York",
+                    data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+                }
+            ]
+        });
+
+        const topRightChart = new Highcharts.Chart({
+            chart: {
+                type: "column",
+                renderTo: "myTopRightChart"
+            },
+            title: {
+                text: "Monthly Average Rainfall"
+            },
+            xAxis: {
+                categories: [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec"
+                ],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: "Rainfall (mm)"
+                }
+            },
+            tooltip: {
+                pointFormat: "{series.name}: <b>{point.y:.1f} mm</b>"
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [
+                {
+                    name: "Tokyo",
+                    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+                }, {
+                    name: "New York",
+                    data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+                }
+            ]
+        });
+
+        const bottomLeftChart = new Highcharts.Chart({
+            chart: {
+                type: "pie",
+                renderTo: "myBottomLeftChart"
+            },
+            title: {
+                text: "Browser market shares January, 2015 to May, 2015"
+            },
+            tooltip: {
+                pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: "pointer",
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [
+                {
+                    name: "Brands",
+                    data: [
+                        {
+                            name: "Microsoft Internet Explorer",
+                            y: 56.33
+                        }, {
+                            name: "Chrome",
+                            y: 24.03
+                        }, {
+                            name: "Firefox",
+                            y: 10.38
+                        }, {
+                            name: "Safari",
+                            y: 4.77
+                        }, {
+                            name: "Opera",
+                            y: 0.91
+                        }, {
+                            name: "Proprietary or Undetectable",
+                            y: 0.2
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const bottomRightChart = new Highcharts.Chart({
+            chart: {
+                type: "pie",
+                renderTo: "myBottomRightChart"
+            },
+            title: {
+                text: "Browser market shares January, 2015 to May, 2015"
+            },
+            tooltip: {
+                pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: "pointer",
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [
+                {
+                    name: "Brands",
+                    data: [
+                        {
+                            name: "Microsoft Internet Explorer",
+                            y: 56.33
+                        }, {
+                            name: "Chrome",
+                            y: 24.03
+                        }, {
+                            name: "Firefox",
+                            y: 10.38
+                        }, {
+                            name: "Safari",
+                            y: 4.77
+                        }, {
+                            name: "Opera",
+                            y: 0.91
+                        }, {
+                            name: "Proprietary or Undetectable",
+                            y: 0.2
+                        }
+                    ]
                 }
             ]
         });
@@ -67,32 +244,32 @@ export class SpendingComponent implements OnInit {
     getFormData() {
         this.userService.getUsers()
             .subscribe(
-                response => this.people = response,
-                error => this.errorMessage = (error as any));
+            response => this.people = response,
+            error => this.errorMessage = (error as any));
 
         this.categoryService.getCategories(true)
             .subscribe(
-                response => this.categories = response,
-                error => this.errorMessage = (error as any));
+            response => this.categories = response,
+            error => this.errorMessage = (error as any));
     }
 
     getTransactions() {
         this.transactionService.getTransactions()
             .subscribe(
-                response => this.transactions = response,
-                error => this.errorMessage = (error as any));
+            response => this.transactions = response,
+            error => this.errorMessage = (error as any));
     }
 
     postCategory(cvm: CategoryViewModel) {
         this.categoryService.postCategory(cvm)
             .subscribe(
-                () => {
-                    if (!this.findCategoryName(cvm.name)) {
-                        this.categories.unshift(cvm.name);
-                    }
-                    this.newCategory();
-                },
-                error => this.errorMessage = (error as any));
+            () => {
+                if (!this.findCategoryName(cvm.name)) {
+                    this.categories.unshift(cvm.name);
+                }
+                this.newCategory();
+            },
+            error => this.errorMessage = (error as any));
     }
 
     findCategoryName(newCatName: string) {
